f10d1b0b12eb0eb995d63d76fa0f5d96e788ce1f
Angelo <ben7152000@gmail.com>
2023-03-01 17:44:35 +0800

    表單開發

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './index.css'
 import { MdOutlineEmail } from "react-icons/md"
 import { RiMessengerLine } from "react-icons/ri"
 import { BsWhatsapp } from "react-icons/bs"
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef()
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    try {
+      await emailjs.sendForm(
+        'service_8s0hl98',
+        'template_zbgdsou',
+        form.current,
+        'jeVc5uXyheImYxqb_'
+      );
+
+      e.target.reset();
+      alert('Your message has been sent!');
+    } catch (error) {
+      alert('An error occurred, please try again later.');
+      console.error(error);
+    }
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -33,10 +55,10 @@ const Contact = () => {
             <a href="https://api.whatsapp.com/send?phone=+886915768987" target='_blank'>Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
-          <textarea name="message"rows="7" placeholder='Your Message' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required />
           <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
       </div>

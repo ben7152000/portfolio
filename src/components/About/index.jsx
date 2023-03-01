be70a22f306f705a648e93ecf4077921fa5a3aea
Angelo <ben7152000@gmail.com>
2023-03-01 11:48:28 +0800

    優化關於

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,6 +4,7 @@ import ME from '../../assets/me-about.jpg'
 import { FaAward } from "react-icons/fa"
 import { FiUsers } from 'react-icons/fi'
 import { VscFolderLibrary } from "react-icons/vsc"
+import Card from './components/Card'
 
 const About = () => {
   return (
@@ -20,23 +21,23 @@ const About = () => {
 
         <div className='about__content'>
           <div className='about__cards'>
-            <article className='about__card'>
-              <FaAward className='about__icon' />
-              <h5>Experience</h5>
-              <small>3+ Years Working</small>
-            </article>
+            <Card
+              icon={<FaAward className='about__icon' />}
+              title='Experience'
+              description='3+ Years Working'
+            />
 
-            <article className='about__card'>
-              <FiUsers className='about__icon' />
-              <h5>Clients</h5>
-              <small>200+ Worldwide</small>
-            </article>
+            <Card
+              icon={<FiUsers className='about__icon' />}
+              title='Clients'
+              description='200+ Worldwide'
+            />
 
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon' />
-              <h5>Projects</h5>
-              <small>80+ Completed</small>
-            </article>
+            <Card
+              icon={<VscFolderLibrary className='about__icon' />}
+              title='Projects'
+              description='80+ Completed'
+            />
           </div>
 
           <p>
